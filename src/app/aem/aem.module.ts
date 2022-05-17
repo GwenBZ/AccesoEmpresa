87ef00f24ff11e779b13f1572db4bf459c43c836
Lenovo <w>
2022-05-17 08:18:17 -0500

    modulo
    Signed-off-by: Lenovo <w>

---
+++ src/app/aem/aem.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AemRoutingModule } from './aem-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AemRoutingModule
+  ]
+})
+export class AemModule { }
