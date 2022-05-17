e2326ca480ae68972b40f83bcbe1290e97ad37a2
MuñozBaezWendyGuadalupe <70306685+GwenBZ@users.noreply.github.com>
2022-05-17 17:10:53 -0500

    Componente Administrar citas
    Signed-off-by: Fabian <w>
    Signed-off-by: MuñozBaezWendyGuadalupe <70306685+GwenBZ@users.noreply.github.com>

--- src/app/aem/aem.module.ts
+++ src/app/aem/aem.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { AemRoutingModule } from './aem-routing.module';
 import { AdministrarcitasComponent } from './administrarcitas/administrarcitas.component';
 import { LoginComponent } from './login/login.component';
+import { Citas2Component } from './citas2/citas2.component';
+import { RegistrarComponent } from './registrar/registrar.component';
 
 
 @NgModule({
   declarations: [
     AdministrarcitasComponent,
-    LoginComponent
+    LoginComponent,
+    Citas2Component,
+    RegistrarComponent
   ],
   imports: [
     CommonModule,
