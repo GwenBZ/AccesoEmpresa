d29962d0577e822038a52cf239806d759fe2adda
Antonio Ramos Hernández <201821336@tese.edu.mx>
2022-05-17 21:25:25 -0500

    RegistrarComponentHTML_TS_y_2imagenesAssets

--- src/app/aem/registrar/registrar.component.ts
+++ src/app/aem/registrar/registrar.component.ts
@@ -11,5 +11,14 @@ export class RegistrarComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  url="./assets/us.png";
+  onselectFile({e}: { e: any }){
+    if (e.target.files){
+      var reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onload=(event:any)=>{
+        this.url=event.target.result;
+      }
+    }
+  }
 }
