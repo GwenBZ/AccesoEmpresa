5baadff2834ebd365ec77b986cf039efbceef355
Daniela09022018 <201821575@tese.edu.mx>
2022-05-20 03:00:32 -0500

    borrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AemComponent } from './recover/aem/aem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AemComponent
   ],
   imports: [
     BrowserModule
