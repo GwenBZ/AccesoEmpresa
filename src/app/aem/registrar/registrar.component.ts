10fbaf42debd9446c6f6acf113eda704d8ed3274
Antonio Ramos Hernández <201821336@tese.edu.mx>
2022-05-29 17:20:12 -0500

    Validaciones en los iputs de hotmail, password, primer nombre, apellidos, y numero de empleado en registrar.component.html, y registrar.component.ts

--- src/app/aem/registrar/registrar.component.ts
+++ src/app/aem/registrar/registrar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-registrar',
@@ -6,19 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrar.component.css']
 })
 export class RegistrarComponent implements OnInit {
+  datos : any;
+  url="./assets/us.png";
 
-  constructor() { }
-
+  constructor() {
+    this.InicializaDatos();
+  }
   ngOnInit(): void {
   }
-  url="./assets/us.png";
+
   onselectFile({e}: { e: any }){
     if (e.target.files){
-      var reader = new FileReader();
+      let reader = new FileReader();
       reader.readAsDataURL(e.target.files[0]);
       reader.onload=(event:any)=>{
         this.url=event.target.result;
       }
     }
+  }
+
+  InicializaDatos(){
+    this.datos = new FormGroup({
+      nombre : new FormControl('@hotmail.com',[Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\\.+com$')]),
+      password : new FormControl("123"),
+      //postal: new FormControl("0", [Validators.pattern('[0-9]*'), Validators.maxLength(4)]),
+      firstName : new FormControl('Adán',[Validators.pattern('^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$')]),
+      apellido : new FormControl('-',[Validators.pattern('^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$')]),
+      numeroEmpleado: new FormControl('0', [Validators.min(1),Validators.max(9999999)])
+
+    });
   }
 }
