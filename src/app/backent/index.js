241a408eef5417183f6f1bcacce73969c4024c4b
Alexanderp07 <105404930+Alexanderp07@users.noreply.github.com>
2022-06-25 20:00:10 -0500

    db
    backent

---
+++ src/app/backent/index.js
@@ -0,0 +1,10 @@
+console.debug('servidor');
+const express=require('express');
+const app=express();
+const port=process.env.port || 5000;
+
+app.set('port',port);
+app.listen(app.set('port'));
+app.use('/login',require('./Router/rutas'));
+
+//http://localhost:5000/login/reg
