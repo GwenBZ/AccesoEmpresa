51f73740a502eaa5ad7a87f41356b81ce5919555
Daniel Hernandez Vergara <93678208+Dany6556S@users.noreply.github.com>
2022-06-28 00:15:06 -0500

    creacion de codigo

--- src/app/loginbackend/config/conexion_bd.js
+++ src/app/loginbackend/config/conexion_bd.js
@@ -1,6 +1,8 @@
 const mysqle=require("mysql");
 const conecta = mysqle.createConnection({
-
-  }
-
-)
+  host:'localhost',
+  user:'root',
+  password:'55Cartel',
+  port:3306,
+  database:'Base',
+})
