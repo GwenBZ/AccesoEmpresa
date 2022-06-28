b2dcf34e06fa883d31958da2fe586fc389fc594d
Daniel Hernandez Vergara <93678208+Dany6556S@users.noreply.github.com>
2022-06-28 00:16:55 -0500

    creacion de codigo

--- src/app/loginbackend/routes/rutas.js
+++ src/app/loginbackend/routes/rutas.js
@@ -4,3 +4,15 @@ const consulta=require('../configuracion/conexion_db');
 routes.get('/reg',(req,res)=>{
   res.send('ruta login');
 });
+
+//Componente Registro a Usuarios
+
+routes.get('/bd',(require,res)=> {
+  let sql = "Select * from registrar"
+  consulta.query(sql, (err, rows) => {
+    if (!err) res.json(rows)
+    else
+      console.error(err,"ERROR-FAVOR DE VERIFICAR CODIGO");
+
+  });
+});
