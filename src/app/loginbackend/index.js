4f5d0232c16be2d07d7365b8144defc92ce2007d
Daniel Hernandez Vergara <93678208+Dany6556S@users.noreply.github.com>
2022-06-28 00:13:15 -0500

    creacion de codigo

--- src/app/loginbackend/index.js
+++ src/app/loginbackend/index.js
@@ -5,3 +5,9 @@ const cors = require('cors');
 
 app.use(express.json())
 app.use(cors)
+
+app.set('port',port);
+app.listen(app.set('port'));
+console.log('Conexión exitosa');
+
+app.use('/login', require('./routes/rutas'));
