4888818e1e33b0166c8c088077e426fc85415656
Daniela09022018 <201821575@tese.edu.mx>
2022-07-02 14:24:24 -0500

    Dany Castro
    Modificacion Roles

--- src/app/aem/recover/recover.component.ts
+++ src/app/aem/recover/recover.component.ts
@@ -1,5 +1,6 @@
 // @ts-ignore
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 // @ts-ignore
 @Component({
@@ -8,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recover.component.css']
 })
 export class RecoverComponent implements OnInit {
+  private Email: any;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+   onselectFile({e}: { e: any }){
+    if (e.target.files){
+      let reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onload=(event:any)=>{
+        this.Email=event.target.result;
+      }
+    }
+  }
+
+  InicializaDatos(){
+    this.Email = new FormGroup({
+      nombre : new FormControl('@hotmail.com',[Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\\.+com$')]),
+      password : new FormControl("123"),
+      //postal: new FormControl("0", [Validators.pattern('[0-9]*'), Validators.maxLength(4)]),
+      firstName : new FormControl('Adán',[Validators.pattern('^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$')]),
+      apellido : new FormControl('-',[Validators.pattern('^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$')]),
+      numeroEmpleado: new FormControl('0', [Validators.min(1),Validators.max(9999999)])
+
+    });
+  }
+
+
+
+
 
 }
