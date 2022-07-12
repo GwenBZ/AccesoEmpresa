902f00c1c5684df0c7e83750c52b4919f059f4da
Antonio Ramos Hernández <201821336@tese.edu.mx>
2022-07-11 23:27:55 -0500

    Consultar, Crear y eliminar Empleado, eñ Componente empleado remplazado por el componente cita, y cambiar la palabra equipo por empleado.

--- src/app/aem/aem.module.ts
+++ src/app/aem/aem.module.ts
@@ -6,11 +6,10 @@ import { AdministrarcitasComponent } from './administrarcitas/administrarcitas.c
 import { LoginComponent } from './login/login.component';
 import { Citas2Component } from './citas2/citas2.component';
 import { RegistrarComponent } from './registrar/registrar.component';
-import { Citas3Component } from './citas3/citas3.component';
-import { Empresas3Component } from './empresas3/empresas3.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SolicitarCitaComponent } from './solicitar-cita/solicitar-cita.component';
-import { BorrarComponent } from './borrar/borrar.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
+import { EditarEmpleadoComponent } from './editar-empleado/editar-empleado.component';
 
 
 @NgModule({
@@ -19,11 +18,10 @@ import { BorrarComponent } from './borrar/borrar.component';
     LoginComponent,
     Citas2Component,
     RegistrarComponent,
-    Citas3Component,
-    Empresas3Component,
     NavbarComponent,
     SolicitarCitaComponent,
-    BorrarComponent
+    EmpleadoComponent,
+    EditarEmpleadoComponent
   ],
   imports: [
     CommonModule,
