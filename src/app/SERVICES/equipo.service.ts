5fc2a01f448125247ce13cde8d7535a462fc2d88
Daniel Hernandez Vergara <93678208+Dany6556S@users.noreply.github.com>
2022-07-11 22:16:52 -0500

    Mostrar Datos en Frontend

---
+++ src/app/SERVICES/equipo.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EquipoService {
+  url='http://localhost:4500/login/bd/log2';
+  constructor(private http: HttpClient) { }
+}
+//OBTENER DATOS
+
+getDatos()
+{
+  return this.http.get(this.url);
+}
