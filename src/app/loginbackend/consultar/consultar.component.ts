e70d83dca6fdead978aeef6145ff1717d2aca7bb
Daniel Hernandez Vergara <93678208+Dany6556S@users.noreply.github.com>
2022-07-11 22:37:20 -0500

    Mostrar Datos en Frontend

--- src/app/loginbackend/consultar/consultar.component.ts
+++ src/app/loginbackend/consultar/consultar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {EquipoService,Datos} from '../../SERVICES/equipo.service'
 
 @Component({
   selector: 'app-consultar',
