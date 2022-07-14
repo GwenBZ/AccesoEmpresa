849b3f9af7118cf7e0d143318b1d9e6172d702d1
MuñozBaezWendyGuadalupe <70306685+GwenBZ@users.noreply.github.com>
2022-07-13 22:15:01 -0500

    depa

---
+++ src/app/aem/emp-departamento/emp-departamento.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EmpDepartamentoComponent } from './emp-departamento.component';
+
+describe('EmpDepartamentoComponent', () => {
+  let component: EmpDepartamentoComponent;
+  let fixture: ComponentFixture<EmpDepartamentoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EmpDepartamentoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EmpDepartamentoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/aem/user-departamento/user-departamento.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserDepartamentoComponent } from './user-departamento.component';
+
+describe('UserDepartamentoComponent', () => {
+  let component: UserDepartamentoComponent;
+  let fixture: ComponentFixture<UserDepartamentoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UserDepartamentoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserDepartamentoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
