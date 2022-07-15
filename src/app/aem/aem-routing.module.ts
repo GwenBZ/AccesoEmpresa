4656a0ae1df05e2e92647332592d3a7d2c0de4f7
MuñozBaezWendyGuadalupe <70306685+GwenBZ@users.noreply.github.com>
2022-07-14 21:52:17 -0500

    rutas

--- src/app/aem/aem-routing.module.ts
+++ src/app/aem/aem-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Departamento', component: DepartamentoComponent },
+  { path: 'Departamento/:id', component: DepartamentoComponent },
+  { path: 'edit/:id', component: ModificarDepartamentoComponent },
+  { path: 'EmpleadoDepartamentos', component: EmpDepartamentoComponent },
+  { path: 'add', component: AddDepartamentoComponent },
+  { path: 'UsuarioDepartamentos', component: UserDepartamentoComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
